fa26c86523b2403965bf9dac13da8dfd80a7179e
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-11-21 15:44:55 +0100

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PetMatch');
+    expect(compiled.querySelector('h1')?.textContent).toContain('PetMatch');
+  });
+
+  it('should have as title "PetMatch"', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('PetMatch');
   });
 });
