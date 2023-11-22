8cc4e62da5cf3807a0ab97620c71e1986ebaa73f
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-11-22 09:51:03 +0100

    initial commit

---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  year: string;
+
+  constructor() {
+    this.year = new Date().getFullYear().toString()
+  }
+
+  ourCompany() {
+    return "switchfully";
+  }
+}
