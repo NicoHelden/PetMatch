b682b10163e194349a4d07fd34dd85fceff98fe6
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-11-23 14:27:47 +0100

    initial commit

--- src/app/profile-gallery/profile-gallery.component.ts
+++ src/app/profile-gallery/profile-gallery.component.ts
@@ -3,18 +3,33 @@ import { Observable } from 'rxjs';
 import { Pet } from '../models/pet';
 import { PetService } from '../services/pet.service';
 import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {NameFilterPipe} from "../pipes/name-filter.pipe";
 
 @Component({
   selector: 'app-profile-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NameFilterPipe],
   templateUrl: './profile-gallery.component.html',
   styleUrls: ['./profile-gallery.component.css']
 })
-export class ProfileGalleryComponent {
-  pets$: Observable<Pet[]>; // Observable of pets
+export class ProfileGalleryComponent implements OnInit {
+  pets$: Observable<Pet[]>; // Use the $ suffix to denote an Observable. This is the observable that the async pipe will subscribe to
+  selectedPet: Pet | null = null; // For storing the selected pet
+  searchTerm: string = ''; // new search term property
 
   constructor(private petService: PetService) {
-    this.pets$ = this.petService.getPets(); // Directly assign the observable from the service
+    this.pets$ = this.petService.getPets(); // This is the observable that the async pipe will subscribe to
   }
+
+  ngOnInit(): void {
+    // If you need to do something with the pets data immediately when the component loads, you can subscribe here
+    // Otherwise, the async pipe in the template will handle the subscription
+  }
+
+  // Method to handle selecting a pet
+  selectPet(pet: Pet): void {
+    this.selectedPet = pet;
+  }
+
 }
--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {map, Observable} from 'rxjs';
 import { Pet } from '../models/pet';
-import {environment} from "../../environments/environment";// make sure the path is correct
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
   apiUrl: string;
-  constructor( private http: HttpClient) {
+
+  constructor(private http: HttpClient) {
     console.log("KVS petservice")
     this.apiUrl = `${environment.backendUrl}/pets`;
   }
 
   getPets(): Observable<Pet[]> {
-    return this.http.get<Pet[]>(this.apiUrl);
+    return this.http.get<Pet[]>(this.apiUrl).pipe(
+      map(pets => pets.sort((a, b) => a.name.localeCompare(b.name)))
+    );
   }
 }
